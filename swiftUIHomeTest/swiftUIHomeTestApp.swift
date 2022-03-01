2ec380720791160bc1c5babda411fb1e3fe3016b
BezginEvgenii <bezgineg@yandex.ru>
2022-03-01 11:12:50 +0300

    first commit

---
+++ swiftUIHomeTest/swiftUIHomeTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUIHomeTestApp.swift
+//  swiftUIHomeTest
+//
+//  Created by Евгений on 01.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct swiftUIHomeTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
